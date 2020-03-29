09ca5ab9eae847ef792af6ffb739413e6cb55a96
sansid <sansid@192.168.0.108>
2020-03-29 19:53:50 +0530

    updated

--- ReadSelenium/src/test/java/tests/SauceLabsDemo.java
+++ ReadSelenium/src/test/java/tests/SauceLabsDemo.java
@@ -7,8 +7,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SauceLabsDemo {
 
-	public final static String USERNAME = "avlearn";
-	public final static String ACCESS_KEY = "a80b80f4-add3-4f1d-b0b9-6bed88425794";
+	public final static String USERNAME = "av-learn";
+	public final static String ACCESS_KEY = "a80b80f4-add3-4dfl-b0b9-6bed88425794";
 	public final static String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
 
 	public static void main(String[] args) throws Exception {
